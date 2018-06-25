612373eced055e59d221253292b68568db79e753
rokicki <rokicki@gmail.com>
2018-06-25 10:14:37 -0700

    Added an implementation of Schreier-Sims.  But don't use it; it's too slow.

--- quat/PuzzleGeometry.js
+++ quat/PuzzleGeometry.js
@@ -1443,6 +1443,94 @@ Perm.prototype = {
       return 0 ;
    },
 }
+function schreiersims(g) {
+   var n = g[0].p.length ;
+   var e = Perm.prototype.e(n) ;
+   var sgs = [] ;
+   var sgsi = [] ;
+   var sgslen = [] ;
+   var Tk = [] ;
+   var Tklen = [] ;
+   function resolve(p) {
+      for (var i=p.p.length-1; i>=0; i--) {
+         var j = p.p[i] ;
+         if (j != i) {
+            if (!sgs[i][j])
+               return false ;
+            p = p.mul(sgsi[i][j]) ;
+         }
+      }
+      return true ;
+   }
+   function knutha(k, p, len) {
+      Tk[k].push(p) ;
+      Tklen[k].push(len) ;
+      for (var i=0; i<sgs[k].length; i++)
+         if (sgs[k][i])
+            knuthb(k, sgs[k][i].mul(p), len+sgslen[k][i]) ;
+   }
+   function knuthb(k, p, len) {
+      var j = p.p[k] ;
+      if (!sgs[k][j]) {
+         sgs[k][j] = p ;
+         sgsi[k][j] = p.inv() ;
+         sgslen[k][j] = len ;
+         for (var i=0; i<Tk[k].length; i++)
+            knuthb(k, p.mul(Tk[k][i]), len+Tklen[k][i]) ;
+         return ;
+      }
+      var p2 = p.mul(sgsi[k][j]) ;
+      if (!resolve(p2))
+         knutha(k-1, p2, len+sgslen[k][j]) ;
+   }
+   function getsgs() {
+      sgs = [] ;
+      sgsi = [] ;
+      Tk = [] ;
+      sgslen = [] ;
+      Tklen = [] ;
+      for (var i=0; i<n; i++) {
+         sgs.push([]) ;
+         sgsi.push([]) ;
+         sgslen.push([]) ;
+         Tk.push([]) ;
+         Tklen.push([]) ;
+         sgs[i][i] = e ;
+         sgsi[i][i] = e ;
+         sgslen[i][i] = 0 ;
+      }
+      var avgs = [] ;
+      var none = 0 ;
+      for (var i=0; i<g.length; i++) {
+         knutha(n-1, g[i], 1) ;
+         var sz = 1 ;
+         var tks = 0 ;
+         var sollen = 0 ;
+         var avgs = [] ;
+         var mults = [] ;
+         for (var j=0; j<n; j++) {
+            var cnt = 0 ;
+            var lensum = 0 ;
+            for (var k=0; k<n; k++)
+               if (sgs[j][k]) {
+                  cnt++ ;
+                  lensum += sgslen[j][k] ;
+                  if (j != k)
+                     none++ ;
+               }
+            tks += Tk[j].length ;
+            sz *= cnt ;
+            if (cnt > 1)
+               mults.push(cnt) ;
+            var avg = lensum / cnt ;
+            avgs.push(avg) ;
+            sollen += avg ;
+         }
+         console.log("After adding " + i + " sz is " + sz + " T " + tks + " sollen " + sollen + " none " + none + " mults " + mults) ;
+      }
+   }
+   getsgs() ;
+}
 if (typeof(process) !== 'undefined' &&
     process.argv && process.argv.length >= 3) {
    var cuts = [] ;
@@ -1454,7 +1542,10 @@ if (typeof(process) !== 'undefined' &&
    console.log("Stickers " + pg.stickersperface + " cubies " +
                pg.cubies.length + " orbits " + pg.orbits +
                 " shortedge " + pg.shortedge) ;
-// pg.writegap() ;
+// var moves = pg.getcookedmoveperms() ;
+// var g = moves.map(function(m){return m[0]}) ;
+// schreiersims(g) ;
+   pg.writegap() ;
 // pg.writess() ;
-   pg.writeksolve() ;
+// pg.writeksolve() ;
 }
